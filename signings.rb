25a29dd38bc338a44033350cc4d471d7ed28279f
Jay <jpnappy@gmail.com>
2016-02-14 23:33:13 -0500

    included sinatra practice

---
+++ signings.rb
@@ -0,0 +1,6 @@
+require 'pry'
+
+sign_ins.each do |sign_in|
+	binding.pry
+	sign_in[0][1]
+end 
---
+++ signins.rb
@@ -0,0 +1,15 @@
+blog_post = {
+
+	title: 'What Makes A Star Starry? Is It Me?',
+	body: "Notice what Tyler Nordgren does in these posters. He's an artist, an astronomer (from Cornell, Carl Sagan's department);",
+	author: {
+
+		first_name: 'Robert',
+		last_name: 'Krulwich',
+		twitter_handle: '@rkrulwich'
+
+	},
+	
+	tags: ['art', 'education', 'science']
+
+}
